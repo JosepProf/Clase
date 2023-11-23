db4adb2192f8a0fe22830753523e7702edd941db
JosepProf <151824143+JosepProf@users.noreply.github.com>
2023-11-23 09:29:54 +0100

    Add files via upload

---
+++ Argumentos/src/main/java/com/mycompany/argumentos/Argumentos.java
@@ -0,0 +1,40 @@
+package com.mycompany.argumentos;
+
+/**
+ *
+ * @author josep
+ */
+public class Argumentos {
+
+    public static void main(String[] args) {
+       
+        String argumento1;
+        String argumento2;
+        
+        argumento1 = args[0];
+        //argumento2 = args[1];
+        
+        System.out.println("Argumento 1: " + argumento1);
+      //  System.out.println("Argumento 2: " + argumento2);
+        
+        switch(argumento1){
+            
+            case "1":{
+                System.out.println("Idioma Español");
+                break;
+            }
+            
+            case "2": {
+                System.out.println("Hi! i'm Muzzy");
+                break;
+            }
+            
+            default: {
+                System.out.println("Argumento incorrecto");
+            }
+            
+        }
+        
+        
+    }
+}
